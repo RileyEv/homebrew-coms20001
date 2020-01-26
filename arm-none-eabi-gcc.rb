9463275e41c80569ca1324970f5a611d323f043f
Riley Evans <riley@rileyevans.co.uk>
2020-01-26 12:04:54 +0000

    Update SHA256 hash

--- arm-none-eabi-gcc.rb
+++ arm-none-eabi-gcc.rb
@@ -6,7 +6,7 @@ class ArmNoneEabiGcc < Formula
   version '5-2015-q4-major'
 
   url 'https://launchpad.net/gcc-arm-embedded/5.0/5-2015-q4-major/+download/gcc-arm-none-eabi-5_2-2015q4-20151219-mac.tar.bz2'
-  sha256 '1249f860d4155d9c3ba8f30c19e7a88c5047923cea17e0d08e633f12408f01f0'
+  sha256 '41056ffeba4bcb5bbea13185461a1269613ac13321fbda3e7dc59ee664ee3f06'
 
   def install
     (prefix/"gcc").install Dir["./*"]
